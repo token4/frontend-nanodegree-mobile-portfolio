8bc41cb745c4eb7a73b25cf84db3c4f89e1053d8
token4 <thomas.j.kunjappu@gmail.com>
2016-02-15 22:22:13 -0800

    re-animate

--- views/js/main.js
+++ views/js/main.js
@@ -503,12 +503,12 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  /*
+  
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
-*/ 
+
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
