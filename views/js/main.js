149311ad2bc595d65bb711b2c2549aec33a6ffb4
token4 <thomas.j.kunjappu@gmail.com>
2016-02-15 22:09:23 -0800

    stop pizza animations

--- views/js/main.js
+++ views/js/main.js
@@ -508,7 +508,7 @@ function updatePositions() {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
-*/
+*/ 
   // User Timing API to the rescue again. Seriously, it's worth learning.
   // Super easy to create custom metrics.
   window.performance.mark("mark_end_frame");
