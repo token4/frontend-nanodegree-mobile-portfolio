c6d99d585aa34b11ad651a7b90d04b8dbd0c4f1c
token4 <thomas.j.kunjappu@gmail.com>
2016-02-16 18:06:46 -0800

    Move sine var outside of loop for pizza animation

--- views/js/main.js
+++ views/js/main.js
@@ -488,9 +488,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-
+  var sine = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(sine + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
